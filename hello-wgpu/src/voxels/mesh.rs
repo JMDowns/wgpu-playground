fe565a6b923c6bcb109d7cc1fcc02e027bf0b523
Jonathan <jmdowns00@gmail.com>
2022-08-03 01:35:39 -0500

    Added face culling

--- hello-wgpu/src/voxels/mesh.rs
+++ hello-wgpu/src/voxels/mesh.rs
@@ -27,7 +27,7 @@ impl Mesh {
         }
     }
 
-    pub fn _stupid(chunk: &Chunk, _solid_data: [[[bool; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2]) -> Self {
+    pub fn _stupid(chunk: &Chunk, solid_data: [[[bool; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2]) -> Self {
 
         let mut mesh = Mesh::new();
 
@@ -35,10 +35,12 @@ impl Mesh {
             for j in 0..CHUNK_DIMENSION as usize {
                 for i in 0..CHUNK_DIMENSION as usize {
                     let block = chunk.get_block_at(i, j, k);
+                    let ambient_occlusion_on_vertices = generate_ambient_occlusion_on_vertices(&solid_data, i+1, j+1, k+1);
+                    let adjacent_blocks_data = generate_adjacent_blocks(&solid_data, i+1, j+1, k+1);
                     if block.get_block_type() != BlockType::AIR {
                         mesh.add_vertices(
-                            generate_cube(chunk.get_absolute_coords_usize(i, j, k), block.get_texture_coords(), &[[0;3];8]), 
-                            generate_cube_indices()
+                            generate_cube(chunk.get_absolute_coords_usize(i, j, k), block.get_texture_coords(), &ambient_occlusion_on_vertices, &adjacent_blocks_data), 
+                            generate_cube_indices(&adjacent_blocks_data)
                         );
                     }
                     
@@ -57,11 +59,12 @@ impl Mesh {
             for j in 0..CHUNK_DIMENSION as usize {
                 for i in 0..CHUNK_DIMENSION as usize {
                     let block = chunk.get_block_at(i, j, k);
-                    let ambient_occlusion_on_vertices = generate_ambient_occlusion_on_vertices(&solid_data, i, j, k);
+                    let ambient_occlusion_on_vertices = generate_ambient_occlusion_on_vertices(&solid_data, i+1, j+1, k+1);
+                    let adjacent_blocks_data = generate_adjacent_blocks(&solid_data, i+1, j+1, k+1);
                     if block.get_block_type() != BlockType::AIR {
                         mesh.add_vertices(
-                            generate_cube(chunk.get_absolute_coords_usize(i, j, k), block.get_texture_coords(), &ambient_occlusion_on_vertices), 
-                            generate_cube_indices()
+                            generate_cube(chunk.get_absolute_coords_usize(i, j, k), block.get_texture_coords(), &ambient_occlusion_on_vertices, &adjacent_blocks_data), 
+                            generate_cube_indices(&adjacent_blocks_data)
                         );
                     }
                     
@@ -231,10 +234,18 @@ impl Mesh {
     }
 }
 
+fn generate_adjacent_blocks(solid_data: &[[[bool; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2], i: usize, j: usize, k: usize) -> [bool; 6] {
+    let mut adjacency_data = [false;6];
+    adjacency_data[0] = solid_data[i-1][j][k];
+    adjacency_data[1] = solid_data[i+1][j][k];
+    adjacency_data[2] = solid_data[i][j][k-1];
+    adjacency_data[3] = solid_data[i][j][k+1];
+    adjacency_data[4] = solid_data[i][j+1][k];
+    adjacency_data[5] = solid_data[i][j-1][k];
+    adjacency_data
+}
+
 fn generate_ambient_occlusion_on_vertices(solid_data: &[[[bool; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2]; CHUNK_DIMENSION as usize+2], i: usize, j: usize, k: usize) -> [[u8;3];8] {
-    let i = i+1;
-    let j = j+1;
-    let k = k+1;
     [
         [
             generate_ambient_occlusion_for_vertex(solid_data[i-1][j][k-1], solid_data[i-1][j-1][k], solid_data[i-1][j-1][k-1]),
@@ -287,8 +298,10 @@ fn generate_ambient_occlusion_for_vertex(is_side_1_solid: bool, is_side_2_solid:
     return is_side_1_solid as u8 + is_side_2_solid as u8 + is_corner_solid as u8;
 }
 
-pub fn generate_cube(pos: Position, tex_coords_arr: &[TextureCoordinates; 6], ambient_occlusion_on_vertices: &[[u8;3]; 8]) -> [Vec<Vertex>; 6] {
+pub fn generate_cube(pos: Position, tex_coords_arr: &[TextureCoordinates; 6], ambient_occlusion_on_vertices: &[[u8;3]; 8], adjacent_blocks_data: &[bool;6]) -> [Vec<Vertex>; 6] {
     let positions = pos.generate_vertex_positions();
+    let mut vertices_arr = [Vec::new(),Vec::new(),Vec::new(),Vec::new(),Vec::new(),Vec::new(),];
+    let all_vertices_arr = 
     [
         [
         Vertex::new(positions[0], tex_coords_arr[0].offset(0.0, TEXTURE_HEIGHT), ambient_occlusion_on_vertices[0][0] as f32),
@@ -326,10 +339,20 @@ pub fn generate_cube(pos: Position, tex_coords_arr: &[TextureCoordinates; 6], am
         Vertex::new(positions[4], tex_coords_arr[5].offset(0.0, TEXTURE_HEIGHT), ambient_occlusion_on_vertices[4][2] as f32),
         Vertex::new(positions[5], tex_coords_arr[5].offset(TEXTURE_WIDTH, TEXTURE_HEIGHT), ambient_occlusion_on_vertices[5][2] as f32),
         ].to_vec()
-    ]
+    ];
+
+    for i in 0..6 {
+        if !adjacent_blocks_data[i] {
+            vertices_arr[i] = all_vertices_arr[i].clone();
+        }
+    }
+
+    vertices_arr
 }
 
-pub fn generate_cube_indices() -> [Vec<u32>;6] {
+pub fn generate_cube_indices(adjacent_blocks_data: &[bool;6]) -> [Vec<u32>;6] {
+    let mut indices_arr = [Vec::new(),Vec::new(),Vec::new(),Vec::new(),Vec::new(),Vec::new(),];
+    let all_indices_arr = 
     [
         // Front Face
         [
@@ -361,5 +384,13 @@ pub fn generate_cube_indices() -> [Vec<u32>;6] {
         1,0,3,
         0,2,3
         ].to_vec(),
-    ]
+    ];
+
+    for i in 0..6 {
+        if !adjacent_blocks_data[i] {
+            indices_arr[i] = all_indices_arr[i].clone();
+        }
+    }
+
+    indices_arr
 }
